8a55effac9365dcc54f0b0ebfb8e727116a59ab9
Saana <t0pusa00@students.oamk.fi>
2022-12-21 20:10:50 +0200

    Random pokemon generator. Axios calls to fetch certain amount of random Pokemon.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import xtype from 'xtypejs'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemons, setPokemon] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [randomOffset, setRandomOffset] = useState(0);
+
+  const fetchPokemon = async () => {
+    const pokemonResponse = await axios(
+      `https://pokeapi.co/api/v2/pokemon/?offset=${randomOffset}&limit=${amount}`
+    );
+    const pokemonData = pokemonResponse.data.results;
+    if(xtype.type(pokemonData) === 'array'){
+      setPokemon(pokemonData);
+    }
+    else{
+      console.log("Something went wrong.")
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, [randomOffset]);
+
+  const handleClick = () => {
+    setRandomOffset(Math.floor(Math.random()*1000));
+  };
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    if(value >= 0){
+      setAmount(value);
+    }
+  };
+
+  if (pokemons.length > 0) {
+    return (
+      <div className="App">
+        <h1>How many pokemons do you want to summon?</h1>
+        <input type="number" value={amount} min={0} onChange={handleChange}></input>
+        <button onClick={handleClick}>Summon!</button>
+        <ol>
+          {pokemons.map((item) => (
+            <li key={item.name}>{item.name}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Oops! Couldn't fetch any pokemons...</p>
+      </div>
+    );
+  }
 }
 
 export default App;
